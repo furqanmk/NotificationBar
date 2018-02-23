0d991141e5a16d53efbbfaec6f6d3d0a2bcb8cfe
Furqan Khan <furqanmk@outlook.com>
2018-02-23 10:42:44 +0100

    Update NotificationBar.swift

--- Source/NotificationBar.swift
+++ Source/NotificationBar.swift
@@ -3,7 +3,7 @@
 //
 // The MIT License (MIT)
 //
-// Copyright (c) 2016 Furqan Muhammad Khan <furqanmk9>
+// Copyright (c) 2018 Furqan Muhammad Khan <furqanmk9>
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
